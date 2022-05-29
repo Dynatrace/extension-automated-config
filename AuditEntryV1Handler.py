0794dca10f1edc45bfdb6777babc5c9b63fd1e77
Aaron Philipose <aaron.philipose@dynatrace.com>
2022-05-29 09:57:43 -0400

    Making PG logic work with V1

--- AuditEntryV1Handler.py
+++ AuditEntryV1Handler.py
@@ -3,7 +3,7 @@ from AuditEntryBaseHandler import AuditEntryBaseHandler
 
 class AuditEntryV1Handler(AuditEntryBaseHandler):
     def extract_info(self, audit_log_entry, request_handler : 'RequestHandler'):
-        annotation_data = super().extract_info(audit_log_entry)
+        annotation_data = super().extract_info(audit_log_entry, request_handler)
         entityId = str(audit_log_entry['entityId']).rsplit(maxsplit=1)[1]
         entityType = str(audit_log_entry['entityId']).split(maxsplit=1)[0]
         annotation_data ['entityId'] = f"\"{entityId}\""
@@ -11,6 +11,7 @@ class AuditEntryV1Handler(AuditEntryBaseHandler):
         # If entityId beings with ME_ then proceed to extract the real entityId by replacing the match with nothing
         if entityType.startswith("ME_PROCESS_GROUP:") and annotation_data['properties']['user'] != "agent quotas worker":
             pgi_list = self.get_processes_from_group(entityId, request_handler)
-            annotation_data ['entityId'] = pgi_list
-        elif entityType.startswith("ME_") and annotation_data['properties']['user'] != "agent quotas worker":
+            pgi_str = self.process_group_instance_to_entity_str(pgi_list)
+            annotation_data ['entityId'] = pgi_str
+        if entityType.startswith("ME_") and annotation_data['properties']['user'] != "agent quotas worker":
             return annotation_data
